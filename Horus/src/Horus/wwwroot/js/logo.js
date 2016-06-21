06a9e250f9972e7fa3e9a5dea6c176dcebec8f9c
Adler <a01630908@itesm.mx>
2016-06-20 23:08:20 -0500

    Register Design Complete

---
+++ Horus/src/Horus/wwwroot/js/logo.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$("#logoImg").css({"height":$(window).height()*0.2 + "px"});
+});
+
+$(window).resize(function(){
+   $("#logoImg").css({"height":$(window).height()*0.2 + "px"}); 
+});
+
